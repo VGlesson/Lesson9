91260b9f5110b777bd35b3a361c77a2c80c890b3
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-10-25 22:05:40 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует калькулятор");
+            Console.WriteLine(' ');
+            double x, y;
+            Console.WriteLine("Введите первое число");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(' ');
+            try
+            {
+                Console.WriteLine(@"Выбирете код операции");
+                Console.WriteLine(@"1-сложение");
+                Console.WriteLine(@"2-вычетание");
+                Console.WriteLine(@"3-произведение");
+                Console.WriteLine(@"4-частное");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message,"неверный код операции");
+            }
+            string q = Console.ReadLine();
+            Console.WriteLine(' ');
+            if (q == "1")
+            {
+                Console.WriteLine(" = {0}", x + y);
+            }
+            if (q == "2")
+            {
+                Console.WriteLine(" = {0}", x + y);
+            }
+            if (q == "3")
+            { 
+                 Console.WriteLine(" = {0}", x * y);
+            }
+            try
+            {
+                if (q == "4")
+                    Console.WriteLine(" = {0}", x / y);
+            }
+            catch (DivideByZeroException ex) when (x==0 && y==00)
+            {
+                Console.WriteLine("Попытка деления на ноль!");
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
